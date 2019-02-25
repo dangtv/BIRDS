a0f122c3bcba42520a87942e57c591923ca2c917
Vandang Tran <dangtv18@gmail.com>
2019-02-25 14:40:19 +0900

    minor update

--- examples.sh
+++ examples.sh
@@ -3,6 +3,6 @@ bin/birds -f examples/basic_sample.dl -o examples/basic_sample.sql -v;
 bin/birds -f examples/music/tracks2.dl -o examples/music/tracks2.sql -v;
 bin/birds -f examples/music/tracks3.dl -o examples/music/tracks3.sql -v;
 
-bin/birds -f examples/ride_sharing/real_peer1_public.dl -o examples/ride_sharing/real_peer1_public.sql;
-bin/birds -f examples/ride_sharing/real_peer2_public.dl -o examples/ride_sharing/real_peer2_public.sql;
-bin/birds -f examples/ride_sharing/real_all_vehicles.dl -o examples/ride_sharing/real_all_vehicles.sql;
+bin/birds -f examples/ride_sharing/real_peer1_public.dl -o examples/ride_sharing/real_peer1_public.sql -v;
+bin/birds -f examples/ride_sharing/real_peer2_public.dl -o examples/ride_sharing/real_peer2_public.sql -v;
+bin/birds -f examples/ride_sharing/real_all_vehicles.dl -o examples/ride_sharing/real_all_vehicles.sql -v;
