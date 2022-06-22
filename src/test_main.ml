5e92a10bbbdecbe3f15c738e63c69a2282d682f3
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2022-06-23 01:05:18 +0900

    refactor tests

--- src/test_main.ml
+++ src/test_main.ml
@@ -3,7 +3,16 @@ open Utils
 open Expr
 
 
-let make_colnamtab (defs : (table_name * column_name list) list) : colnamtab =
+type table_definition = table_name * column_name list
+
+type test_case = {
+  tables   : table_definition list;
+  rule     : rule;
+  expected : string;
+}
+
+
+let make_colnamtab (defs : table_definition list) : colnamtab =
   let colnamtab : colnamtab = Hashtbl.create 32 in
   defs |> List.iter (fun (table, cols) ->
     let arity = List.length cols in
@@ -12,34 +21,11 @@ let make_colnamtab (defs : (table_name * column_name list) list) : colnamtab =
   colnamtab
 
 
-let main () =
+let single (test_case : test_case) =
   let open ResultMonad in
-  let colnamtab =
-    make_colnamtab [
-      ("ed", ["emp_name"; "dept_name"]);
-      ("eed", ["emp_name"; "dept_name"]);
-    ]
-  in
-
-  (* "+ed(E, D) :- ed(V1, D), E = 'Joe', D = 'A', V1 != 'Joe', ¬ed(E, D), ¬eed(V1, D)." *)
-  let rule =
-    let head = Deltainsert ("ed", [ NamedVar "E"; NamedVar "D" ]) in
-    let body =
-      [
-        Rel (Pred ("ed", [ NamedVar "V1"; NamedVar "D" ]));
-        Equat (Equation ("=", Var (NamedVar "E"), Const (String "'Joe'")));
-        Equat (Equation ("=", Var (NamedVar "D"), Const (String "'A'")));
-        Equat (Equation ("<>", Var (NamedVar "V1"), Const (String "'Joe'")));
-        Not (Pred ("ed", [ NamedVar "E"; NamedVar "D" ]));
-        Not (Pred ("eed", [ NamedVar "V1"; NamedVar "D" ]));
-      ]
-    in
-    (head, body)
-  in
-
-  let expected =
-    "SELECT 'Joe' AS emp_name, 'A' AS dept_name FROM ed AS ed0 WHERE ed0.dept_name = 'A' AND ed0.emp_name <> 'Joe' AND NOT EXISTS ( SELECT * FROM ed AS t WHERE t.emp_name = 'Joe' AND t.dept_name = 'A' ) AND NOT EXISTS ( SELECT * FROM eed AS t WHERE t.emp_name = ed0.emp_name AND t.dept_name = 'A' )"
-  in
+  let colnamtab = make_colnamtab test_case.tables in
+  let rule = test_case.rule in
+  let expected = test_case.expected in
 
   Ast2sql.convert_to_operation_based_sql colnamtab rule >>= fun sql ->
   let got = Ast2sql.stringify_sql_query sql in
@@ -56,6 +42,31 @@ let main () =
 
 
 let () =
-  match main () with
+  let test_case =
+    {
+      tables = [
+        ("ed", ["emp_name"; "dept_name"]);
+        ("eed", ["emp_name"; "dept_name"]);
+      ];
+      (* "+ed(E, D) :- ed(V1, D), E = 'Joe', D = 'A', V1 != 'Joe', ¬ed(E, D), ¬eed(V1, D)." *)
+      rule =
+        (Deltainsert ("ed", [ NamedVar "E"; NamedVar "D" ]), [
+          Rel (Pred ("ed", [ NamedVar "V1"; NamedVar "D" ]));
+          Equat (Equation ("=", Var (NamedVar "E"), Const (String "'Joe'")));
+          Equat (Equation ("=", Var (NamedVar "D"), Const (String "'A'")));
+          Equat (Equation ("<>", Var (NamedVar "V1"), Const (String "'Joe'")));
+          Not (Pred ("ed", [ NamedVar "E"; NamedVar "D" ]));
+          Not (Pred ("eed", [ NamedVar "V1"; NamedVar "D" ]));
+        ]);
+      expected =
+        String.concat " " [
+          "SELECT 'Joe' AS emp_name, 'A' AS dept_name FROM ed AS ed0 WHERE";
+          "ed0.dept_name = 'A' AND ed0.emp_name <> 'Joe' AND";
+          "NOT EXISTS ( SELECT * FROM ed AS t WHERE t.emp_name = 'Joe' AND t.dept_name = 'A' ) AND";
+          "NOT EXISTS ( SELECT * FROM eed AS t WHERE t.emp_name = ed0.emp_name AND t.dept_name = 'A' )";
+        ]
+    }
+  in
+  match single test_case with
   | Ok ()   -> ()
   | Error _ -> ()
